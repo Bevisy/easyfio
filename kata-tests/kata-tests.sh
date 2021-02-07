22572db5a8e781459ad18b8e99279ba2fff7ae9e
Binbin Zhang <binbin36520@gmail.com>
2021-02-07 14:44:58 +0800

    fix tiny error

--- kata-tests/kata-tests.sh
+++ kata-tests/kata-tests.sh
@@ -13,7 +13,7 @@ mkdir -p results
 # bs=8k iodepth=1
 ./run-fio-test.sh $TESTNAME-8k-1 $TESTDIR fio-jobs/* --size="8k" --iodepth="1" > results/fio-results-$TESTNAME-8k-1.txt
 # bs=8k iodepth=128
-./run-fio-test.sh $TESTNAME-8k-1 $TESTDIR fio-jobs/* --size="8k" --iodepth="128" > results/fio-results-$TESTNAME-8k-128.txt
+./run-fio-test.sh $TESTNAME-8k-128 $TESTDIR fio-jobs/* --size="8k" --iodepth="128" > results/fio-results-$TESTNAME-8k-128.txt
 # bs=1024k iodepth=1
 ./run-fio-test.sh $TESTNAME-1M-1 $TESTDIR fio-jobs/* --size="1024k" --iodepth="1" > results/fio-results-$TESTNAME-1M-1.txt
 # bs=1024k iodepth=16
