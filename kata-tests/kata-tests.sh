0bf286baa9220c0b1fe3b30e8f9b787426a0f378
bevisy <binbin36520@gmail.com>
2023-10-26 19:25:11 +0800

    update: add blocksize 4M test

--- kata-tests/kata-tests.sh
+++ kata-tests/kata-tests.sh
@@ -18,3 +18,9 @@ mkdir -p results
 ./run-fio-test.sh $TESTNAME-1M-1 $TESTDIR fio-jobs/* --iodepth="1" --blocksize="1M" > results/fio-results-$TESTNAME-1M-1.txt
 # bs=1024k iodepth=16
 ./run-fio-test.sh $TESTNAME-1M-16 $TESTDIR fio-jobs/* --iodepth="16" --blocksize="1M" > results/fio-results-$TESTNAME-1M-16.txt
+# bs=4096k iodepth=1
+./run-fio-test.sh $TESTNAME-4M-1 $TESTDIR fio-jobs/* --iodepth="1" --blocksize="4M" > results/fio-results-$TESTNAME-4M-1.txt
+# bs=4096k iodepth=16
+./run-fio-test.sh $TESTNAME-4M-16 $TESTDIR fio-jobs/* --iodepth="16" --blocksize="4M" > results/fio-results-$TESTNAME-4M-16.txt
+# bs=4096k iodepth=128
+./run-fio-test.sh $TESTNAME-4M-128 $TESTDIR fio-jobs/* --iodepth="128" --blocksize="4M" > results/fio-results-$TESTNAME-4M-128.txt
